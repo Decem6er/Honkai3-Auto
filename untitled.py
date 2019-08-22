f482e4edeec7770d4574965b62eff70c3b25c3fc
Decem6er <cgmz@live.com>
2019-08-22 10:58:02 +0800

    Honkai3

---
+++ untitled.py
@@ -0,0 +1,59 @@
+# -*- encoding=utf8 -*-
+__author__ = "Xuyy1"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+while 1:
+    if exists(Template(r"tpl1565702806178.png", record_pos=(-0.013, 0.131), resolution=(1440, 810))):
+        touch(Template(r"tpl1565702806178.png", record_pos=(-0.013, 0.131), resolution=(1440, 810)),times=5)
+        break
+sleep(10.0)
+
+touch(Template(r"tpl1565702076235.png", record_pos=(0.29, -0.033), resolution=(1440, 810)))
+while not exists(Template(r"tpl1565702276495.png", record_pos=(0.289, 0.218), resolution=(1440, 810))):
+    swipe(Template(r"tpl1565702375370.png", record_pos=(0.193, 0.263), resolution=(1440, 810)), vector=[-0.1518, -0.0152])
+    if exists(Template(r"tpl1565702276495.png", record_pos=(0.289, 0.218), resolution=(1440, 810))):
+        touch(Template(r"tpl1565702276495.png", record_pos=(0.289, 0.218), resolution=(1440, 810)))
+        break
+else:
+    touch (Template(r"tpl1565702276495.png", record_pos=(0.289, 0.218), resolution=(1440, 810)))
+sleep(1.0)
+touch(Template(r"tpl1565702486971.png", record_pos=(-0.251, -0.024), resolution=(1440, 810)))
+touch(Template(r"tpl1565702500812.png", record_pos=(0.283, 0.155), resolution=(1440, 810)))
+touch(Template(r"tpl1565704878652.png", record_pos=(0.199, -0.028), resolution=(1440, 810)))
+touch(Template(r"tpl1565702522667.png", record_pos=(0.366, 0.244), resolution=(1440, 810)))
+touch(Template(r"tpl1565702537906.png", record_pos=(0.368, 0.235), resolution=(1440, 810)))
+if exists(Template(r"tpl1565702564817.png", record_pos=(0.001, 0.001), resolution=(1440, 810))):
+    touch(Template(r"tpl1565702576311.png", record_pos=(0.219, 0.152), resolution=(1440, 810)))
+sleep(18.0)
+touch(Template(r"tpl1565703544457.png", record_pos=(-0.378, 0.119), resolution=(1440.0, 810.0)),duration=1.0)
+
+touch(Template(r"tpl1565703548118.png", record_pos=(0.443, 0.211), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565703549188.png", record_pos=(0.443, 0.211), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565703550247.png", record_pos=(0.443, 0.211), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565703551355.png", record_pos=(0.443, 0.211), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565703549188.png", record_pos=(0.443, 0.211), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565703550247.png", record_pos=(0.443, 0.211), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565703551355.png", record_pos=(0.443, 0.211), resolution=(1440.0, 810.0)))
+
+touch(Template(r"tpl1565703553006.png", record_pos=(0.442, 0.204), resolution=(1440.0, 810.0)))
+
+touch(Template(r"tpl1565702623080.png", record_pos=(0.469, 0.194), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565702624149.png", record_pos=(0.455, 0.206), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565702624717.png", record_pos=(0.455, 0.206), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565702624149.png", record_pos=(0.455, 0.206), resolution=(1440.0, 810.0)))
+touch(Template(r"tpl1565702624717.png", record_pos=(0.455, 0.206), resolution=(1440.0, 810.0)))
+
+
+
+        
+sleep(12.0)
+touch(Template(r"tpl1565702664897.png", record_pos=(-0.388, 0.241), resolution=(1440.0, 810.0)))
+sleep(6.5)
+touch(Template(r"tpl1565702679912.png", record_pos=(0.327, 0.233), resolution=(1440.0, 810.0)))
+
+
+
+
+
